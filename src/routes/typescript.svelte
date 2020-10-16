f812eea7f64b1f734bc35184c761bddce13f52d9
Gabriel Bodeen <gabebodeen@gmail.com>
2020-10-15 22:45:28 -0500

    update to sapper template with typescript

---
+++ src/routes/typescript.svelte
@@ -0,0 +1,15 @@
+<script lang="typescript">
+  let text: string = "";
+
+  function mutate(content: string): string {
+    return content.toUpperCase().split("").reverse().join("");
+  }
+</script>
+
+<svelte:head>
+  <title>TypeScript support</title>
+</svelte:head>
+
+<textarea placeholder="There's nothing here yet" bind:value={text} />
+
+<p>{mutate(text)}</p>
